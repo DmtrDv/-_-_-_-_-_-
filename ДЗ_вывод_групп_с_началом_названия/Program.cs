742eba73d1e5d81b253bcce1a2da6bc2796379e1
DmtrDv <dmitriybychkov75146@gmail.com>
2024-10-10 00:30:05 +0300

    модернизировал первый способ. И сделал второй способ поиска

--- ДЗ_вывод_групп_с_началом_названия/Program.cs
+++ ДЗ_вывод_групп_с_началом_названия/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,26 +11,46 @@ namespace ДЗ_вывод_групп_с_началом_названия
     {
         static void Main()
         {
-            List<string> groups = new List<string>() { "П-30", "Б-30", "Э-30", "М-30", "П-31", "Л-30", "П-32" };
-            string sought = "П-3";
-            var targets = groups.Where(groups.Contains(sought,StringComparison.OrdinalIgnoreCase)).ToList();
+
+            /// первый способ
+
+            /*Console.WriteLine("Введите группы которые ищите (с заглавной буквы):");
+            
+            string sought = Console.ReadLine(); //вводим искомое
+            List<string> groups = new List<string>() { "П-30", "Б-30", "Э-30", "М-30", "П-31", "Л-30", "П-32" };  //создал список групп
+
+
+            var targets = groups.Where(gr => gr.Contains(sought)); //вводим переменную targets(цели) и присваиваем ей значени, значение равно: в списке
+                                                                   //спомощью метода where, создания новой переменной gr и метода Contains ищем подходящие элементы
             Console.WriteLine("Найденные элементы: ");
-
-
 
             foreach (var group in targets)
             {
-                Console.WriteLine(group);
+                Console.WriteLine(String.Join(",", group));
             }
+            */
 
 
 
 
+            ///второй способ
 
+            Console.WriteLine("Введите группы которые ищите (с заглавной буквы):");
 
+            string sought = Console.ReadLine(); //вводим искомое
+            List<string> groups = new List<string>() { "П-30", "Б-30", "Э-30", "М-30", "П-31", "Л-30", "П-32" };  //создал список групп
+            
+            Console.WriteLine("Найденные элементы: ");
 
-
+            foreach (var group in groups)
+            {
+                if (group.Contains(sought))
+                {
+                    Console.WriteLine(String.Join(",", group));
+                }
+            }
+            
             Console.ReadKey();
-        }
+        }        
     }
 }
